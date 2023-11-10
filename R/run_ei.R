61be2296379039aa4c319855a7d5111d5a48f631
gidonc <gidon.cohen@durham.ac.uk>
2023-11-10 20:48:44 +0000

    make function to run stan models work for the first time.

--- R/run_ei.R
+++ R/run_ei.R
@@ -1,8 +1,19 @@
-ei_estimate <- function(row_margins, col_margins, model){
+ei_estimate <- function(row_margins, col_margins, model, iter = 2000){
 
   R <- ncol(row_margins)
   C <- ncol(col_margins)
+  n_areas <- nrow(row_margins)
 
-  out <- rstan::sampling(stanmodel$ssMND,
-                         data = standata)
+  standata <- list(
+    n_areas = n_areas,
+    R = R,
+    C = C,
+    row_margins = row_margins,
+    col_margins = col_margins
+  )
+
+  out <- rstan::sampling(stanmodels$ssMND,
+                         data = standata,
+                         cores = 4,
+                         iter = iter)
 }
